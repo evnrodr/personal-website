d7417f9de19101ff45b28b204f54d39fa4ed58c5
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-07-11 18:14:38 -0300

    🌟 initializing the project, first files and configurations

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+        <meta name="description" content="Homepage de Evandro Rodrigues" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
